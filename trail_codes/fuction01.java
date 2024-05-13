2faaba680dbc5541072b7f20af96962683219d60
royisten <nemihidomine@gmail.com>
2024-05-13 21:49:03 +0530

    13/5/24(3)

---
+++ trail_codes/fuction01.java
@@ -0,0 +1,37 @@
+package trail_codes;
+
+import java.util.Scanner;
+
+public class fuction01 {
+    public static void main (String []args){
+        Scanner sca= new Scanner(System.in);
+        int prompt;
+        int Y = 0;
+        
+        System.out.println("press 0 for addition & 1 for to get a wink");
+        prompt=sca.nextInt();
+        if (prompt==Y) {
+            twosum();
+        } else {
+            System.out.println(";)");
+            
+        }
+        
+       
+        
+        sca.close();
+    }
+
+    public static int  twosum(){
+        Scanner sca= new Scanner(System.in);
+        int num1,num2;
+        System.out.println("first value");
+        num1=sca.nextInt();
+        System.out.println("Second value");
+        num2=sca.nextInt();
+        int result= num1+num2;
+        System.out.println("the result is "+ result );
+        sca.close();
+        return 0;
+    }
+}
