8f59dab763b66b9efdb32511e65ce3314f8912fe
royisten <nemihidomine@gmail.com>
2024-05-16 17:47:38 +0530

    16/5/24(3)

--- JavaOOPS/testoops.java
+++ JavaOOPS/testoops.java
@@ -6,14 +6,14 @@ public class testoops {
    static int b=25; // static variable b initialized to 25
 
     public static void main(String test1[]){
-        testoops h =new testoops();
-        testoops h1 = new testoops();
-        h.b = 122;// modifies the static variable b to 122
-        h1.b=125;// modifies the same static variable b to 125
-        b=2550;// directly modifies the static variable b to 2550
+        //testoops h =new testoops();
+        //testoops h1 = new testoops();
+        //h.b = 122;// modifies the static variable b to 122
+        //h1.b=125;// modifies the same static variable b to 125
+        //b=2550;// directly modifies the static variable b to 2550
 
-        System.out.println(h.b); // prints the current value of b, which is 2550
-    
+        //System.out.println(h.b); // prints the current value of b, which is 2550
+        
         /* In Java, a static variable belongs to the class rather than any instance of the class. 
         This means that there is only one copy of the static variable, regardless of how many instances of the class are created.
         When you modify a static variable, it affects the single shared copy.
