1d7e9d45abe36645d4c7d0bd17db760ca3dad0d9
royisten <nemihidomine@gmail.com>
2024-05-16 18:26:04 +0530

    16/5/24(6)

---
+++ constructor_java/constructor01.java
@@ -0,0 +1,10 @@
+package constructor_java;
+// Class with a constructor that prints a message
+public class constructor01 {
+  // Constructor of Constructor01 class 
+  constructor01(){
+     // Print message when the constructor is called
+      System.out.println("ho ho ho");
+    }
+    
+}
---
+++ constructor_java/main_to_call_construtor.java
@@ -0,0 +1,11 @@
+package constructor_java;
+// Main class to call the constructor of Constructor01
+public class main_to_call_construtor {
+    public static void main (String samplework[]){
+        // Create an instance of Constructor01 which triggers its constructor
+        constructor01 hi = new constructor01();
+        
+        
+    }
+    
+}
