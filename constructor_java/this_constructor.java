0fa23ff2cd0254e44629c182d62c834217f090aa
royisten <nemihidomine@gmail.com>
2024-05-17 08:47:14 +0530

    17/5/24[2]

--- constructor_java/this_constructor.java
+++ constructor_java/this_constructor.java
@@ -3,17 +3,30 @@ package constructor_java;
 import java.util.Scanner;
 
 public class this_constructor {
+    // Declaring instance variables a1 and b1
     int a1,b1;
+    // Constructor of the class which takes two integer parameters k and l
     this_constructor(int k, int l){
+         // Local variable m to store the sum of k and l
         int m;
+         // Calculating the sum of k and l and assigning it to m
         m=k+l;
+         // Printing the sum of k and l
         System.out.println(m);
+         // Using 'this' keyword to refer to the current object's instance variables
         this.a1=k;
         this.b1=l;
-    } public void sum (){
+         // Printing the values of instance variables a1 and b1
+        System.out.println(a1+""+b1);
+    }
+    // Method to calculate and print the sum of instance variables a1 and b1
+     public void sum (){
         Scanner s = new Scanner(System.in);
+        // Local variable y to store the sum of a1 and b1
         int y;
+         // Calculating the sum of instance variables a1 and b1 and assigning it to y
         y = this.a1 +this.b1;
+         // Printing the sum of a1 and b1
         System.out.print(y);
         s.close();
     }
--- constructor_java/thistest01.java
+++ constructor_java/thistest01.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class thistest01 {
     public  static void main (String usingTHIS[]){
+         // Creating an object 'o' of the class 'this_constructor' with parameters 6 and 5
         this_constructor o = new this_constructor(6, 5);
+        // Calling the 'sum' method on the created object to print the sum of instance variables
         o.sum();
         Scanner s = new Scanner(System.in);
         System.out.print("\n");
