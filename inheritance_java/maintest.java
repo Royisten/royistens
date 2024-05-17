a9ec301ee650432d2acb58736fcfdfe41461c953
royisten <nemihidomine@gmail.com>
2024-05-17 19:38:49 +0530

    17/5/24[4]

--- inheritance_java/maintest.java
+++ inheritance_java/maintest.java
@@ -1,13 +1,23 @@
 package inheritance_java;
 
 import java.util.Scanner;
-
+//Test showing that a method can be called from an another class,
+//Using inheritance   
 public class maintest {
     public static void main (String onetwo[]){
-        Scanner s = new Scanner(System.in);
+        
+      Scanner s = new Scanner(System.in);
+      
+        //object "inherit" created of  class "test02".
       test02 inherit = new test02();
+      //here, "display01" method belongs to class "test01" but can be called using the object of "test02" ,
+      //since "test02" inherited "test01"
       inherit.display01();
+      //calling method from class  "test02" using oops.
       inherit.display02();
+      //displaying values that are declared in both classes "test01 ; a" and 
+      // "test02 ; m" which can be called directly using the object of class "test02"
+      //since "test02" inherited "test01"
       System.out.println("test 01 : "+inherit.a+ "\n" +"test 02 : "+ inherit.m);
       s.close();
       
