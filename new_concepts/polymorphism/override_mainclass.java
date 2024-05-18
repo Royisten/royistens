254d6779b362a524d0204c1ef1ae1e5d7aa48e53
royisten <nemihidomine@gmail.com>
2024-05-18 22:24:23 +0530

    18/5/24[6]

--- new_concepts/polymorphism/override_mainclass.java
+++ new_concepts/polymorphism/override_mainclass.java
@@ -4,6 +4,7 @@ public class override_mainclass {
     public static void main(String overridetestMain[]){
         override_subclass test = new override_subclass();
         test.display();
+        
      // here we created a instance of the sub class which has also inherited the base class ,
      // but when we called the method "display()" which is present both in sub AND base class.
      // the output was " Sub class" which belongs to the sub class method 
--- new_concepts/polymorphism/override_subclass.java
+++ new_concepts/polymorphism/override_subclass.java
@@ -5,4 +5,6 @@ public class override_subclass extends override_baseclass {
     void display(){
         System.out.println("Sub class");
     }
-}
+   
+    }
+
