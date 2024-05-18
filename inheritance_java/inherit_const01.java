4ce005c5f49d88309e7a5b56f3625847bca45367
royisten <nemihidomine@gmail.com>
2024-05-18 09:16:00 +0530

    18/5/24{3}

--- inheritance_java/inherit_const01.java
+++ inheritance_java/inherit_const01.java
@@ -1,7 +1,7 @@
 package inheritance_java;
 
-public class inherit_const01 {
+public class inherit_const01 extends inherit_const02 {
     inherit_const01 (){
-        System.out.println("hi 02");
+        System.out.println("hi 01");
     }
 }
--- inheritance_java/inherit_const_main.java
+++ inheritance_java/inherit_const_main.java
@@ -2,9 +2,14 @@ package inheritance_java;
 
 public class inherit_const_main {
     public static void main (String ToShowConstructor[]){
-        //inherit_const01 c01 = new inherit_const01();
-        //inherit_const02 c02 = new inherit_const02();
+       // inherit_const01 c01 = new inherit_const01();
+       
          
     }
 }
-/* this is to show that we can also inherit constructor of */
+/* this is to show that we can also inherit constructor and also 
+ * the base class will run first since "inherit_const01 extends inherit_const02" and 
+ * the result will be 
+ * 02 (inherit_const02)
+ * 01 (inherit_const01)
+*/
