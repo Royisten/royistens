336d8a5082ad29eaa3baeb037f5b47516c5cef7e
royisten <nemihidomine@gmail.com>
2024-05-22 21:10:17 +0530

    22/5/24{1}

---
+++ trail_codes/add01testclone.java
@@ -0,0 +1,11 @@
+package trail_codes;
+
+public class add01testclone {
+    public static void main(String []args){
+        int no1 = 5;
+        int no2 = 6;
+        int value = no1 + no2;
+        System.out.println(value);
+
+    }
+}
