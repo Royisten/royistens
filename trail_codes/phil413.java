c194df75edc84bf9a756b314aac124b417a97590
royisten <nemihidomine@gmail.com>
2024-05-28 11:13:17 +0530

    28/5/24 1

--- trail_codes/phil413.java
+++ trail_codes/phil413.java
@@ -4,7 +4,7 @@ public class phil413 {
     
         public static void main(String[] args) {
             System.out.println("\nI can do all things through him who strengthens me");
-            System.out.println("Philippians 4:13 ");
+            System.out.println(" \"Philippians 4:13\" ");
         
     }
     }
